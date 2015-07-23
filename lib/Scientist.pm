16647852e3af2f5144ed8ccac09afec52140c2cc
Joshua Keroes <joshua.keroes@dreamhost.com>
2015-07-23 16:16:59 -0700

    minil new

---
+++ lib/Scientist.pm
@@ -0,0 +1,39 @@
+package Scientist;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Scientist - It's new $module
+
+=head1 SYNOPSIS
+
+    use Scientist;
+
+=head1 DESCRIPTION
+
+Scientist is ...
+
+=head1 LICENSE
+
+Copyright (C) Joshua Keroes.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Joshua Keroes E<lt>joshua.keroes@dreamhost.comE<gt>
+
+=cut
+
